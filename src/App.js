3cac85baab1b8b80ee4256c28c6dd36a2426a5e3
Benoit-Ferrer <ferrer.benoit@reezocar.com>
2021-01-07 17:02:38 +0100

    scss

--- src/App.js
+++ src/App.js
@@ -9,19 +9,6 @@ function App() {
     <div className="App">
        <ExampleSass />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.dsd
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- src/sass.js
+++ src/sass.js
@@ -3,10 +3,10 @@ import "./sass.scss";
 export const ExampleSass = () => {
   return (
     <div className="container">
-      <div className="sass-title"> Un titre Sass</div>
+      <div className="sass-title"> Un titre Sass direct dans le container</div>
       <div className="content">
-          J'suis son parent
-        <div className="inContent message">Je suis dans le content</div>
+          J'suis son parent mais l'enfant du container
+        <div className="inContent message">Je suis dans le content qui lui est dans le container</div>
       </div>
       <div className="box"/>
     </div>
